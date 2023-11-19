09040b42591ce0dee5e65cf3bebca2b6e15ee5f7
Margarita Zinoveeva <m@zinoveeva.ru>
2023-11-19 21:15:03 +0300

    End of work

--- best_game/best_game.py
+++ best_game/best_game.py
@@ -32,24 +32,24 @@ def random_predict(number: int = 1) -> int:
     return count
 
 
-def score_game(random_predict) -> int:
+def score_game(random_predict) -> float:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
 
     Returns:
-        int: среднее количество попыток
+        float: среднее количество попыток
     """
     count_ls = []
-    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    score = round(np.mean(count_ls), 3)
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
 
